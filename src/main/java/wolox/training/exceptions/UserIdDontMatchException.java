cf00394f31dbc487493a458c38d9faad14916d25
Leonel Gutierrez <gonzalo.gutierrez@wolox.com.ar>
2019-12-30 12:39:14 -0300

    Add user controller and exceptions Added user controller with crud operations and book operations from user, also added the common exceptions for user

---
+++ src/main/java/wolox/training/exceptions/UserIdDontMatchException.java
@@ -0,0 +1,11 @@
+package wolox.training.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE, reason = "User id don't match")
+public class UserIdDontMatchException extends Exception {
+    public UserIdDontMatchException() {
+        super();
+    }
+}
