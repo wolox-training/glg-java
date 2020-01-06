53b14bf8a200488bba7b807a60ca04109e95930e
Leonel Gutierrez <gonzalo.gutierrez@wolox.com.ar>
2020-01-06 16:49:47 -0300

    Update runtime exception

--- src/main/java/wolox/training/exceptions/UserIdDontMatchException.java
+++ src/main/java/wolox/training/exceptions/UserIdDontMatchException.java
@@ -4,9 +4,4 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE, reason = "User id don't match")
-public class UserIdDontMatchException extends Exception {
-
-    public UserIdDontMatchException() {
-        super();
-    }
-}
+public class UserIdDontMatchException extends RuntimeException {}
--- src/main/java/wolox/training/exceptions/UserNotFoundException.java
+++ src/main/java/wolox/training/exceptions/UserNotFoundException.java
@@ -4,9 +4,4 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found")
-public class UserNotFoundException extends Exception {
-
-    public UserNotFoundException() {
-        super();
-    }
-}
+public class UserNotFoundException extends RuntimeException {}
